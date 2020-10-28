cfec30ccd1a28a09ac416c40a44db062508a25b2
Crowdbotics <team@crowdbotics.com>
2020-10-28 11:06:51 +0000

    add package blueprint-react-native-blank-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import CopyOfCopyOfBlankScreen5164029Navigator from '../features/CopyOfCopyOfBlankScreen5164029/navigator';
 import CopyOfCopyOfBlankScreen5164028Navigator from '../features/CopyOfCopyOfBlankScreen5164028/navigator';
 import CopyOfCopyOfBlankScreen5164027Navigator from '../features/CopyOfCopyOfBlankScreen5164027/navigator';
 import CopyOfCopyOfBlankScreen5164026Navigator from '../features/CopyOfCopyOfBlankScreen5164026/navigator';
@@ -55,6 +56,7 @@ import Maps158796Navigator from '../features/Maps158796/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+CopyOfCopyOfBlankScreen5164029: { screen: CopyOfCopyOfBlankScreen5164029Navigator },
 CopyOfCopyOfBlankScreen5164028: { screen: CopyOfCopyOfBlankScreen5164028Navigator },
 CopyOfCopyOfBlankScreen5164027: { screen: CopyOfCopyOfBlankScreen5164027Navigator },
 CopyOfCopyOfBlankScreen5164026: { screen: CopyOfCopyOfBlankScreen5164026Navigator },
